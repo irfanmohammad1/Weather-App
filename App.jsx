399bde9c6911bdb3a184cbf3aa1db97b466447da
Mohammad Irfan <133006092+irfanmohammad1@users.noreply.github.com>
2024-09-07 17:40:50 +0530

    Create App.jsx
    App.jsx

---
+++ App.jsx
@@ -0,0 +1,22 @@
+//import './App.css';
+
+// import SearchBox from "./SearchBox";
+// import InfoBox from "./InfoBox";
+import WeatherApp from "./WeatherApp";
+
+function App() {
+  return (
+    <>
+    <WeatherApp/>
+    
+    {/* <SearchBox/>
+    <InfoBox/> */}
+    {/* <SearchBox/> */}
+    
+   
+   </>
+  );
+};
+
+export default App;
+  
